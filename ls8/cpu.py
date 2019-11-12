98ba1011ccc3156bde908b970ee678ab2f9b1776
PascalUlor <pascalulor@yahoo.com>
2019-11-12 18:59:08 +0100

    read file from example folder

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -25,15 +25,36 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
+        program = []
+        with open(sys.argv[1]) as f:
+            for line in f:
+            # deal with comments
+            # split before and after any comment symbol '#'
+                comment_split = line.split("#")
+
+            # convert the pre-comment portion (to the left) from binary to a value
+            # extract the first part of the split to a number variable
+            # and trim whitespace
+                num = comment_split[0].strip()
+
+            # ignore blank lines / comment only lines
+                if len(num) == 0:
+                    continue
+
+            # set the number to an integer of base 2
+                value = int(num, 2)
+                program.append(value)
+            # print the value in binary and in decimal
+                print(f"{value:08b}: {value:d}")
 
         for instruction in program:
             self.ram_write(address, instruction)
